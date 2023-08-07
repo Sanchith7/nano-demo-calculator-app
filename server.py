f2a84b280fe2cf67fe17c17b1ac0316669a55ed3
Sanchith .S <sanchith.s1710@gmail.com>
2023-08-07 16:58:38 +0000

    hi

--- server.py
+++ server.py
@@ -4,15 +4,36 @@ app = Flask(__name__)
 
 @app.route('/calculator/greeting', methods=['GET'])
 def greeting():
-    return jsonify({"message": "Hello world!"}), 200
+    return 'Hello world!', 200
 
 @app.route('/calculator/add', methods=['POST'])
 def add():
     data = request.get_json()
-    if 'first' not in data or 'second' not in data:
-        return jsonify({"error": "Both 'first' and 'second' must be provided."}), 400
+    first = data.get('first')
+    second = data.get('second')
+    if first is None or second is None:
+        return jsonify({'error': 'Both "first" and "second" numbers are required.'}), 400
+    try:
+        result = float(first) + float(second)
+        return jsonify({'result': result}), 200
+    except ValueError:
+        return jsonify({'error': 'Invalid input. Both "first" and "second" must be numbers.'}), 400
 
-   
+@app.route('/calculator/subtract', methods=['POST'])
+def subtract():
+    data = request.get_json()
+    first = data.get('first')
+    second = data.get('second')
+    if first is None or second is None:
+        return jsonify({'error': 'Both "first" and "second" numbers are required.'}), 400
+    try:
+        result = float(first) - float(second)
+        return jsonify({'result': result}), 200
+    except ValueError:
+        return jsonify({'error': 'Invalid input. Both "first" and "second" must be numbers.'}), 400
+
+if __name__ == '__main__':
+    app.run()
 
 
 if __name__ == '__main__':
